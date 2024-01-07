f1f517bf4ae48ca132316ecade091bf4a319be67
chanidaphach28 <chanidapha.28chsk@gmail.com>
2024-01-07 21:16:53 +0700

    modified:   components/MarkerMap/map.js 	modified:   components/MarkerMap/mark.module.css 	modified:   package-lock.json 	modified:   package.json 	modified:   pages/NearMe/index.js 	modified:   pages/NearMe/nearme.module.css 	modified:   pages/index.js 	new file:   public/Homepage.jpg 	new file:   public/pinlo.png 	new file:   public/scicon.png 	new file:   public/searchlo.png 	modified:   styles/Home.module.css     หน้า nearme กดได้จาก navbar แสดงแผนที่และค้นหาได้ (ลองค้นหอได้ กมล aj หอพัก ) สร้างช่อง search location+ชื่อหอ ไว้ที่หน้า home & navbar     (#7/1/2024)

--- pages/NearMe/index.js
+++ pages/NearMe/index.js
@@ -1,22 +1,44 @@
-import Style from "../NearMe/nearme.module.css";
+import style from "../NearMe/nearme.module.css";
 import dynamic from "next/dynamic";
+import { useState, useEffect } from "react";
+import Image from 'next/image';
 
-const Map = dynamic(() => import("@/components/MarkerMap/map"),{
-    ssr: false
-})
-
-
+const Map = dynamic(() => import("@/components/MarkerMap/map"), {
+  ssr: false,
+});
 
 export default function Home() {
-  
+  const [searchlocat, setSearchLocat] = useState("");
+
+  const handleSearch = (e) => {
+    setSearchLocat(e.target.value);
+  };
+
   return (
     <>
-        <div className={Style.bor}>
-            <div className={Style.text}>map nearme</div>
-        </div>
+      <div className={style.bor}>
+       
       
-    
-     <Map/>
+
+      <span className={style.search}>
+        <Image src="/pinlo.png" width={25} height={25} alt="img"/>
+      <div class={style.textbox}>KMITL</div>
+      <div class={style.line}></div>
+        <div class = {style.text_andicon}>
+          <input
+          
+            className={style.search_text}
+            type="text"
+            placeholder="Search Dormitory name or Location"
+            onChange={handleSearch}
+            value={searchlocat}
+          />
+          <i class={style.search_icon}><Image src="/searchlo.png" width={20} height={20} alt="img"/></i>
+          
+          </div>
+        </span></div>
+      <Map searchlocat={searchlocat} />
     </>
   );
 }
+
